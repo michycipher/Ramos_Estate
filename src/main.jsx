f6df8fdc577f298cda630000dc34bb4ea434e5d7
michycipher <michelleutomi@yahoo.com>
2025-07-19 18:24:20 +0100

    deployment fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,31 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// // import './index.css'
+// import App from './App.jsx'
+// import "./index.scss"
+// import { AuthContextProvider } from './context/AuthContextProvider.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <AuthContextProvider>
+//     <App />
+//     </AuthContextProvider>
+//   </StrictMode>,
+// )
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
 import App from './App.jsx'
 import "./index.scss"
 import { AuthContextProvider } from './context/AuthContextProvider.jsx'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthContextProvider>
-    <App />
-    </AuthContextProvider>
+    <HashRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </HashRouter>
   </StrictMode>,
 )
